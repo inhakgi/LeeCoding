6a9a9b956b5823b103755d70403b84c7c6c9fb0e
Inhak Lee <hagibal@naver.com>
2022-02-04 18:09:55 +0900

    updated 220204. 18:09

--- test.py
+++ test.py
@@ -2,13 +2,26 @@ import time
 import pyupbit
 import datetime
 
-symbols = ["KRW-MBL", "KRW-EOS", "KRW-MFT", "KRW-CRE", "KRW-TT", "KRW-MVL"]
-target_price = {}
+access = "RKwDQiNOzHkimFdtXaqxL28Nf94DIRwAy7ixzbGX"
+secret = "BhdEHxbux2Oc12ro2VaX5kI7FlfCxidJphcoCiQS"
 
-for symbol in symbols:
-    df = pyupbit.get_ohlcv(symbol, interval="minute5", count=20)
-    k = 1 - (abs(df.iloc[0]['open'] - df.iloc[0]['close']) / (df.iloc[0]['high'] - df.iloc[0]['low']))
-    target_price[symbol] = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
 
-print(target_price)
+# symbols = ["KRW-EOS", "KRW-MBL", "KRW-MFT", "KRW-CRE", "KRW-TT", "KRW-MVL"]
+# target_price = {}
 
+# for symbol in symbols:
+#     df = pyupbit.get_ohlcv(symbol, interval="minute5", count=2)
+#     k = 1 - (abs(df.iloc[-2]['open'] - df.iloc[-2]['close']) / (df.iloc[-2]['high'] - df.iloc[-2]['low']))
+#     target_price[symbol] = df.iloc[-1]['close'] + (df.iloc[-1]['high'] - df.iloc[-1]['low']) * k
+    
+
+# print(target_price)
+
+
+
+
+symbols = ["KRW-EOS", "KRW-MBL", "KRW-MFT", "KRW-CRE", "KRW-TT", "KRW-MVL"]
+coins = pyupbit.get_current_price(symbols)
+print(coins)
